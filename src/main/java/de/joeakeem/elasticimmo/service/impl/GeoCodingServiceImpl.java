372cac9824d259c4407f9b0763a9db6b28d8f58b
joe-akeem <j.lengacher@gmx.net>
2014-12-04 23:18:35 +0100

    added initial implementation of athentication with geo coding service.

--- src/main/java/de/joeakeem/elasticimmo/service/impl/GeoCodingServiceImpl.java
+++ src/main/java/de/joeakeem/elasticimmo/service/impl/GeoCodingServiceImpl.java
@@ -1,13 +1,20 @@
 package de.joeakeem.elasticimmo.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.elasticsearch.common.geo.GeoPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import de.joeakeem.elasticimmo.model.EstateGeo;
 import de.joeakeem.elasticimmo.service.GeoCodingService;
+import de.joeakeem.elasticimmo.wigeogis.model.GETKEY;
 
 /**
  * Provides access to the WiGeoGis geo coding functions.
@@ -17,12 +24,64 @@ import de.joeakeem.elasticimmo.service.GeoCodingService;
  */
 @Service
 public class GeoCodingServiceImpl implements GeoCodingService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(GeoCodingServiceImpl.class);
     
     /**
      * The amount of milliseconds until the a newly received key times out.
      * This is 300 per documentation, with 290 seconds we're on the save side.
      */
-    public long KEY_EXPIRATION_TIMEOUT = 290000L;
+	private static final long KEY_EXPIRATION_TIMEOUT = 290000L;
+    
+    /**
+     * The WiGeoGis account name variable.
+     */
+	private static final String USR = "USR";
+    
+    /**
+     * The WiGeoGis account password variable.
+     */
+	private static final String CID = "CID";
+    
+    /**
+     * The WiGeoGis request type variable.
+     */
+	private static final String TYPE = "TYPE";
+	
+	/**
+	 * Status code of the WiGeoGis response for successfule requests.
+	 */
+	private static final String OK_STATUS = "OK";
+	
+	/**
+	 * The URL of the WiGeoGis authentication resource.
+	 */
+	@Value("${wiGeoGis.authentication.url}")
+	private String authenticationUrl;
+	
+	/**
+	 * The URL of the WiGeoGis geo coding resource.
+	 */
+	@Value("${wiGeoGis.geocode.url}")
+	private String geocodeUrl;
+    
+	/**
+	 * The WiGeoGis account name.
+	 */
+	@Value("${wiGeoGis.usr}")
+    private String usr;
+    
+    /**
+	 * The WiGeoGis account password.
+	 */
+	@Value("${wiGeoGis.cid}")
+    private String cid;
+	
+	/**
+	 * The key of the current WiGeoGis session which is valid
+	 * for <code>KEY_EXPIRATION_TIMEOUT</code> milliseconds until it must be renewed.
+	 */
+	private String key;
 
     /**
      * The time in milliseconds when the current key expires
@@ -66,8 +125,21 @@ public class GeoCodingServiceImpl implements GeoCodingService {
      */
     private void authenticate() {
         if (isKeyExpired()) {
-            // TODO: implement authentication and save session key
-            keyExpirationTime = System.currentTimeMillis() + KEY_EXPIRATION_TIMEOUT;
+        	Map<String, String> urlVariables = new HashMap<String, String>();
+        	urlVariables.put(USR, usr);
+        	urlVariables.put(CID, cid);
+        	urlVariables.put(TYPE, "geocode");
+        	
+        	GETKEY response = wiGeoGisRestTemplate.getForObject(authenticationUrl, GETKEY.class, urlVariables);
+        	
+        	if (OK_STATUS.equals(response.getSTATUS())) {
+        		key = response.getKEY();
+        		keyExpirationTime = System.currentTimeMillis() + KEY_EXPIRATION_TIMEOUT;
+        		LOG.info("Successfully authneticated with the geo coding service.");
+        	} else {
+        		LOG.error("failed to authenticate with the geo coding service: {} (error code: {})",
+        				response.getMESSAGE(), response.getERRORCODE());
+        	}
         }
     }
     
